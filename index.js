56c200a25d366f65af48f45ab9b4a123d49c0771
EstebanIndiveri <esteban.indiveri@gmail.com>
2020-12-04 22:08:19 -0300

    add new findelement

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ const {Builder, By, Key, until} = require('selenium-webdriver');
     await driver.get('http://www.google.com/ncr');
     await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
     await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+    
+    
+    await driver.get('https://www.gogle.com/');
+    await driver.findElement(By.name('q')).sendKeys('selenium',Key.RETURN);
+    await driver.wait(until.titleIs('selenium - gooogle'),1000);
+
   } finally {
     await driver.quit();
   }
